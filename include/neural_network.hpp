5be31e3f53da4d43af4e6a951e0f7ffb479e9f92
Filip Peterek <filip.peterek@firma.seznam.cz>
2022-06-04 21:38:48 +0200

    Neural network progress

--- include/neural_network.hpp
+++ include/neural_network.hpp
@@ -14,8 +14,22 @@ struct Neuron {
 
 
 struct Layer {
+
     std::vector<Neuron> neurons;
+
     Layer(size_t size);
+
+    std::size_t size() const;
+
+    const Neuron & operator[](size_t idx) const;
+    Neuron & operator[](size_t idx);
+
+    std::vector<Neuron>::iterator begin();
+    std::vector<Neuron>::iterator end();
+    std::vector<Neuron>::const_iterator begin() const;
+    std::vector<Neuron>::const_iterator end() const;
+    std::vector<Neuron>::const_iterator cbegin() const;
+    std::vector<Neuron>::const_iterator cend() const;
 };
 
 
@@ -23,7 +37,8 @@ namespace activation {
     typedef std::function<double(double)> Function;
     typedef Function fn;
 
-    double sigmoid(double potential);
+    double sigmoid(double potential, double lambda);
+    fn createSigmoid(double lambda);
 };
 
 
@@ -32,6 +47,11 @@ namespace activ = activation;
 
 class BackpropagationNetwork {
     activ::fn activationFn;
+
+    double lambda = 1.0;
+    double eta = 0.1;
+    double threshold = 0.1;
+    int iterations = 10;
 
     std::vector<Layer> layers;
     std::mt19937 rnd { std::random_device()() };
@@ -42,12 +62,26 @@ class BackpropagationNetwork {
 
     void initLayer(size_t layer);
 
+    void validateInput(const std::vector<double> & input);
+
+    std::vector<double> calcLayerValues(size_t layer, const std::vector<double> & inputs);
+
+    std::vector<std::vector<double>> outputsOfAllLayers(const std::vector<double> & inputs);
+
+    double calcNetworkError(const std::vector<double> & signals, size_t expected);
+
+    double adjustOutputLayer(const std::vector<double> & signals, size_t expected);
+
+    bool teachIteration(const std::vector<double> & signals, size_t expected);
+
 public:
 
     BackpropagationNetwork(activ::fn fun, size_t input, size_t output, size_t hiddenLayers, size_t hiddenLayerNeurons);
 
     size_t predict(const std::vector<double> & input);
-    std::vector<double> outputWeight(const std::vector<double> & input);
+    std::vector<double> outputValues(std::vector<double> input);
+
+    void teach(const std::vector<double> & signals, size_t expected);
 
 };
 
--- src/neural_network.cpp
+++ src/neural_network.cpp
@@ -1,7 +1,60 @@
 #include "neural_network.hpp"
+#include <SFML/Graphics/PrimitiveType.hpp>
+#include <stdexcept>
 
 
-Layer::Layer(size_t size) : layers(size) { }
+Layer::Layer(size_t size) : neurons(size) { }
+
+const Neuron & Layer::operator[](const size_t idx) const {
+    return neurons[idx];
+}
+
+Neuron & Layer::operator[](const size_t idx) {
+    return neurons[idx];
+}
+
+std::size_t Layer::size() const {
+    return neurons.size();
+}
+
+std::vector<Neuron>::iterator Layer::begin() {
+    return neurons.begin();
+}
+
+std::vector<Neuron>::iterator Layer::end() {
+    return neurons.end();
+}
+
+std::vector<Neuron>::const_iterator Layer::begin() const {
+    return neurons.begin();
+}
+
+std::vector<Neuron>::const_iterator Layer::end() const {
+    return neurons.end();
+}
+
+std::vector<Neuron>::const_iterator Layer::cbegin() const {
+    return neurons.cbegin();
+}
+
+std::vector<Neuron>::const_iterator Layer::cend() const {
+    return neurons.cend();
+}
+
+namespace activation {
+
+    double sigmoid(const double potential, const double lambda) {
+        const auto exp = std::exp(-1 * lambda * potential);
+        return 1.0 / (1 + exp);
+    }
+
+    fn createSigmoid(const double lambda) {
+        return [lambda](const double potential) -> double {
+            return sigmoid(potential, lambda);
+        };
+    }
+    
+}
 
 
 BackpropagationNetwork::BackpropagationNetwork(
@@ -33,11 +86,137 @@ void BackpropagationNetwork::initLayer(const size_t layerIdx) {
     const auto weigths = layers[layerIdx-1].neurons.size();
     auto layer = layers[layerIdx];
 
-    for (auto & neuron : layer.neurons) {
+    for (auto & neuron : layer) {
         neuron.weights.reserve(weigths);
         for (size_t i = 0; i < weigths; ++i) {
             neuron.weights.emplace_back(dist(rnd));
         }
     }
 }
+
+
+size_t BackpropagationNetwork::predict(const std::vector<double> & input) {
+
+    const auto out = outputValues(input);
+
+    double maxVal = 0;
+    size_t maxIdx = 0;
+
+    for (size_t i = 0; i != out.size(); ++i) {
+        if (out[i] > maxVal) {
+            maxVal = out[i];
+            maxIdx = i;
+        }
+    }
+
+    return maxIdx;
+}
+
+void BackpropagationNetwork::validateInput(const std::vector<double> & input) {
+    if (input.size() != layers.front().size()) {
+        throw std::runtime_error("Input size does not match number of neurons in network input layer.");
+    }
+}
+
+double BackpropagationNetwork::neuronOutput(const Neuron & neuron, const std::vector<double> & inputs) {
+    return activationFn(neuronPotential(neuron, inputs));
+}
+
+double BackpropagationNetwork::neuronPotential(const Neuron & neuron, const std::vector<double> & inputs) {
+
+    double potential = 0.0;
+
+    for (size_t i = 0; i < neuron.weights.size(); ++i) {
+        potential += neuron.weights[i] * inputs[i];
+    }
+
+    return potential;
+}
+
+std::vector<double> BackpropagationNetwork::calcLayerValues(size_t layer, const std::vector<double> & inputs) {
+
+    const auto & current = layers[layer];
+    std::vector<double> out(current.size(), 0.0);
+
+    for (size_t i = 0; i < current.size(); ++i) {
+        out[i] = neuronOutput(current[i], inputs);
+    }
+
+    return out;
+}
+
+std::vector<double> BackpropagationNetwork::outputValues(std::vector<double> input) {
+    
+    for (size_t i = 1; i < layers.size(); ++i) {
+        auto next = calcLayerValues(i, input);        
+        input = std::move(next);
+    }
+
+    return input;
+}
+
+std::vector<std::vector<double>> BackpropagationNetwork::outputsOfAllLayers(const std::vector<double> & inputs) {
+
+    std::vector<std::vector<double>> outputs;
+    outputs.reserve(layers.size());
+
+    outputs.emplace_back(calcLayerValues(1, inputs));
+
+    for (size_t i = 2; i < layers.size(); ++i) {
+        outputs.emplace_back(calcLayerValues(i, outputs[i-2]));
+    }
+
+    return outputs;
+}
+
+double BackpropagationNetwork::calcNetworkError(const std::vector<double> & signals, const size_t expected) {
+
+    double error = 0;
+    
+    for (size_t i = 0; i < signals.size(); ++i) {
+        const double expVal = (i == expected);
+        const double received = signals[i];
+        const double err = expected - received;
+        error += err*err;
+    }
+
+    return error;
+}
+
+double BackpropagationNetwork::adjustOutputLayer(const std::vector<double> & signals, const size_t expected) {
+    
+    for (size_t i = 0; i < signals.size(); ++i) {
+        const double expVal = (i == expected);
+        const double received = signals[i];
+        const double error = (expected - received) * lambda * received * (1 - received);
+
+        const double dw = eta * error * received;
+        // TODO: Fix
+    }
+
+    return 0.0;
+}
+
+bool BackpropagationNetwork::teachIteration(const std::vector<double> & signals, const size_t expected) {
+
+    auto outputs = outputsOfAllLayers(signals);
+    const auto & outputLayer = outputs.back();
+
+    const double error = calcNetworkError(signals, expected);
+
+    if (error < threshold) {
+        return true;
+    }
+
+    return false;
+}
+
+void BackpropagationNetwork::teach(const std::vector<double> & signals, const size_t expected) {
+    for (
+        int i = 0;
+        i < iterations and not teachIteration(signals, expected);
+        ++i
+    );
+}
+
 
